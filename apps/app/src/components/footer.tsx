c6ee25452115b3a2750c5dab65a02bcd037317e7
Elmata2 <643689jw@eur.nl>
2024-09-10 10:14:38 +0100

    made some small changes in teh footer.tsx fikes

--- apps/app/src/components/footer.tsx
+++ apps/app/src/components/footer.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export function Footer() {
   return (
     <div className="flex items-center justify-center font-mono text-xs fixed bottom-0 right-4 bottom-4 space-x-2">
